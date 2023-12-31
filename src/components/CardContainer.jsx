2c1f325d70b3892d3c51841f4ad9672f0f88525b
owen <jagaaoguz.7000@gmail.com>
2023-12-31 23:41:08 +0300

    versino 1.0

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -3,19 +3,25 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import {data} from '../helpers/data'
 import MapsCard from './MapsCard';
+import { useState } from 'react';
 
 
 
 const CardContainer = () => {
+    const[search,setSearch] = useState("")
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    }
     return(
     <>
         <Form.Control
             placeholder='Search a map'
             className='w-50 m-auto'
+            onChange={handleChange}
         />
         <Container className='rounded-4 my-4 p-3 card-container'>
             <Row className='justify-content-center g-3'>
-                {data.map((maps, i) => (
+                {data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase().trim())).map((maps, i) => (
                     <Col xl={3} lg={4} md={6} key={i}>
                         <MapsCard {...maps}/>
                     </Col>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import headpp from "../assets/headpp.png"
+import arklogo from "../assets/arklogo.png"
 import Image from "react-bootstrap/Image"
 import Container from "react-bootstrap/Container"
 
@@ -7,7 +7,7 @@ import Container from "react-bootstrap/Container"
 const Header = () =>{
     return (
         <Container>
-            <Image src={headpp}></Image>
+            <Image className="map-logo" src={arklogo}></Image>
             <h1 className="my-2 font-monospace display-4 fw-bold">All map of ark</h1>
         </Container>
     )
